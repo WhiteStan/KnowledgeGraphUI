7ad1eb397dae92bbed916034fbffde9704eaa81d
Stanislau Makouski <Stanislau_Makouski@epam.com>
2021-02-03 20:33:48 +0300

    added gitignore and webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, './script.js') 
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].bunde.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  }
+}
